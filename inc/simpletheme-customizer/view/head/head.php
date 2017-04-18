7ed2ce3ca8db5677b2227e7d48d02cd78102ac70
frasaleksander <fongreecss@gmail.com>
2017-04-18 16:22:26 +0200

    update

--- inc/simpletheme-customizer/view/head/head.php
+++ inc/simpletheme-customizer/view/head/head.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php echo $charset; ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <?php wp_head(); ?>
 <?php SimplethemeCustomizer::show('custom_css'); ?>
