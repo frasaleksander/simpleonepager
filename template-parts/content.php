74e43575c2eb875dfe36e5715c610bc62d05ca83
frasaleksander <fongreecss@gmail.com>
2017-04-18 16:19:55 +0200

    update

--- template-parts/content.php
+++ template-parts/content.php
@@ -32,7 +32,7 @@
 				/* translators: %s: Name of current post. */
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'simpletheme' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+			), true );
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'simpletheme' ),
